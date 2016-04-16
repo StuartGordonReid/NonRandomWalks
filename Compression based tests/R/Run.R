50f2bdec1f47bddb2d710b6f2fb5cc4e3a37c5d5
Stuart <stuartgordonreid@gmail.com>
2016-04-17 01:29:33 +0200

    Finished article and code. Committing now

--- Compression based tests/R/Run.R
+++ Compression based tests/R/Run.R
@@ -9,26 +9,86 @@ library(parallel)
 library(PerformanceAnalytics)
 
 
-testRobustness <- function(sd, t) {
-  signal <- sin(seq(1, t))/50
-  noise <- rnorm(t, mean = 0.0, sd = sd)
-  dates <- seq.Date(Sys.Date(), Sys.Date() + t - 1, 1)
-  simdata <- to.levels(signal + noise)
-  simdata <- xts(simdata, order.by = dates)
-  hexdata <- marketData(simdata, detrend = T)
-  print(compress(hexdata))
+runExperiments <- function() {
+  for (algo in c("gzip", "bzip", "xz")) {
+    print(paste("Testing compression algorithm:", algo))
+    
+    # Some liquid market index tests.
+    # indices <- getGlobalStockMarketIndices()
+    # for (ix in indices)
+    #   compressionTest(ix, algo = algo)
+    
+    # Some liquid currency tests.
+    pairs <- getCurrencyPairs()
+    for (fx in pairs)
+      compressionTest(fx, algo = algo)
+  }
 }
 
 
-to.levels <- function(rets) {
-  lseq <- c(1)
-  for (i in 1:length(rets))
-    lseq <- c(lseq, lseq[i - 1] * (1 + rets[i]))
-  return(lseq)
+compressionTest <- function(code, years = 7, algo = "g") {
+  # The generic Quandl API key for TuringFinance.
+  Quandl.api_key("t6Rn1d5N1W6Qt4jJq_zC")
+  
+  # Download the raw price data.
+  data <- Quandl(code, rows = -1, type = "xts", collapse = "daily")
+  
+  # Extract the variable we are interested in.
+  ix.ac <- which(colnames(data) == "Adjusted Close")
+  if (length(ix.ac) == 0) 
+    ix.ac <- which(colnames(data) == "Close")
+  ix.rate <- which(colnames(data) == "Rate")
+  closes <- data[ ,max(ix.ac, ix.rate)]
+  
+  # Get the month endpoints.
+  monthends <- endpoints(closes)
+  monthends <- monthends[2:length(monthends) - 1]
+  
+  # Observed compression ratios.
+  cratios <- c()
+  for (t in ((12 * years) + 1):length(monthends)) {
+    # Extract a window of length equal to years.
+    window <- closes[monthends[t - (12 * years)]:monthends[t]]
+    # Compute detrended log returns.
+    returns <- Return.calculate(window, method = "log")
+    returns <- na.omit(returns) - mean(returns, na.rm = T)
+    # Binarize the returns.
+    returns[returns < 0] <- 0
+    returns[returns > 0] <- 1
+    # Convert into raw hexadecimal.
+    hexrets <- bin2rawhex(returns)
+    # Compute the compression ratio
+    cratios <- c(cratios, length(memCompress(hexrets)) / 
+                   length(hexrets))
+  }
+  
+  # Expected compression ratios.
+  ecratios <- c()
+  for (i in 1:length(cratios)) {
+    # Generate some benchmark returns.
+    returns <- rnorm(252 * years)
+    # Binarize the returns.
+    returns[returns < 0] <- 0
+    returns[returns > 0] <- 1
+    # Convert into raw hexadecimal.
+    hexrets <- bin2rawhex(returns)
+    # Compute the compression ratio
+    ecratios <- c(ecratios, length(memCompress(hexrets)) / 
+                    length(hexrets))
+  }
+  
+  if (mean(cratios) >= min(1.0, mean(ecratios))) {
+    print(paste("Dataset:", code, "is not compressible { c =", 
+                mean(cratios), "} --> efficient."))
+  } else {
+    print(paste("Dataset:", code, "is compressible { c =", 
+                mean(cratios), "} --> inefficient."))
+  }
 }
 
 
 bin2rawhex <- function(bindata) {
+  bindata <- as.numeric(as.vector(bindata))
   lbindata <- split(bindata, ceiling(seq_along(bindata)/4))
   hexdata <- as.vector(unlist(mclapply(lbindata, bin2hex)))
   hexdata <- paste(hexdata, sep = "", collapse = "")
@@ -39,109 +99,136 @@ bin2rawhex <- function(bindata) {
 }
 
 
-benchmarkData <- function(kDays) {
-  bindata <- rbinom(kDays, 1, 0.5)
-  # plotBinData(bindata)
-  return(bin2rawhex(bindata))
-}
-
-
-marketData <- function(closes, detrend = TRUE) {
-  if (detrend) {
-    logreturns <- Return.calculate(closes, method = "log")
-    logreturns <- logreturns - mean(logreturns, na.rm = TRUE)
-    returns <- exp(logreturns) - 1
-  } else {
-    returns <- Return.calculate(closes)
-  }
-
-  charts.PerformanceSummary(returns)
-  returns <- as.numeric(as.vector(returns))
-  returns[is.nan(returns)] <- NA
-  returns[is.infinite(returns)] <- NA
-  returns[returns == 0.0] <- NA
-  returns <- na.omit(returns)
+testRobustness <- function(t = (252 * 7)) {
+  # Generate an underlying signal.
+  signal <- sin(seq(1, t)) / 50
+  signal <- signal - mean(signal)
   
-  bindata <- returns
-  bindata[bindata > 0] <- 1
-  bindata[bindata < 0] <- 0
-  # plotBinData(bindata)
-  return(bin2rawhex(bindata))
-}
-
-
-plotBinData <- function(bindata) {
-  bindata[bindata == 0] <- -1
-  plot.ts(cumsum(bindata))
-}
-
-
-compress <- function(x, method = "g") {
-  y <- switch(method,
-              g = Rcompression::gzip(x),
-              b = brotli_compress(x))
-  return(length(y) / length(x))
-}
-
-
-monteCarloSim <- function(kDays, sims = 30) {
-  benchmarks <- list()
-  for (i in 1:sims)
-    benchmarks[[i]] <- benchmarkData(kDays)
-  compressions <- mclapply(benchmarks, compress)
-  return(unlist(compressions))
-}
-
-
-run <- function(index = "YAHOO/INDEX_GSPC", years = 5, collapse = "daily", type = "real") {
-  # Download the data from Quandl.com
-  pricedata <- Quandl(index, rows = -1, collapse = collapse, 
-                      end_date = "2015-12-31")
-  # Extract the dates as a POSIX variable.
-  dates <- as.Date(pricedata$"Date")
-  
-  # Extract the relevant time series data.
-  if (length(grep("YAHOO", index)) > 0)
-    ts <- as.xts(pricedata$"Adjusted Close", order.by = dates)
-  else if (length(grep("CURRFX", index)) > 0)
-    ts <- as.xts(pricedata$"Rate", order.by = dates)
-  else # Assume we want to use the second column.
-    ts <- as.xts(pricedata[ ,2], order.by = dates)
-  
-  # Get the split positions.
-  pos <- endpoints(ts, on = "years") + 1
-  pos <- pos[seq(1, length(pos) - 1)]
-  
-  # If we have enough sub samples.
-  if (ceiling(length(pos) / years) > 3) {
-    pos <- pos[seq(years, length(pos), years)]
-    ts.subs <- splitAt(ts, pos)
-    ts.subs.dates <- c(index(ts.subs[[1]])[1])
-    for (i in 2:length(ts.subs))
-      ts.subs.dates <- c(ts.subs.dates, index(ts.subs[[i]])[1])
-    ts.hexdata <- lapply(ts.subs, marketData)
-    ts.compressions <- unlist(lapply(ts.hexdata, compress))
-    ts.xts <- xts(ts.compressions[seq(2, length(ts.compressions))], 
-                  order.by = as.Date(ts.subs.dates[seq(2, length(ts.subs.dates))]))
-    plot(ts.xts, main = "Subsequence Compression Ratios")
-  } else {
-    print(paste("Compression Achieved:", compress(marketData(ts))))
+  # For different noise levels
+  sds <- seq(0.0, 0.020, 0.0005)
+  cratios <- c()
+  for (s in sds) {
+    # Generate a noisy signal
+    noise <- rnorm(t, mean = 0, sd = s)
+    returns <- signal + noise
+    # Binarize the data.
+    returns[returns < 0] <- 0
+    returns[returns > 0] <- 1
+    # Convert into raw hexadecimal.
+    hexrets <- bin2rawhex(returns)
+    # Compute the compression ratio
+    cratios <- c(cratios, length(memCompress(hexrets)) / 
+                   length(hexrets))
+    
+    # Plot the returns for the eye ball test.
+    dates <- seq.Date(Sys.Date(), Sys.Date() + (252 * 7) - 1, 1)
+    # plot.ts(xts(signal + noise, order.by = dates))
+    
+    if (s %in% c(0.010, 0.015, 0.020)) {
+      charts.PerformanceSummary(xts(signal + noise, order.by = dates))
+    }
   }
   
-  # Plot the mean compression on random data.
-  mc.means <- c()
-  for (sub in ts.subs)
-    mc.means <- c(mc.means, mean(monteCarloSim(length(sub))))
-  mc.xts <- xts(mc.means[seq(2, length(mc.means))], 
-                order.by = as.Date(ts.subs.dates[seq(2, length(ts.subs.dates))]))
-  lines(mc.xts, col = "red")
+  # Plot the compression ratios.
+  plot(sds, cratios)
 }
 
 
-splitAt <- function(x, pos) {
-  unname(split(x, cumsum(seq_along(x) %in% pos)))
+bettingStrategyProof <- function(t = (252 * 7), w = 5, sd = 0.02) {
+  # Generate an underlying signal.
+  signal <- sin(seq(1, t)) / 50
+  signal <- signal - mean(signal)
+  
+  # Add some noise to get a return series.
+  returns <- signal + rnorm(t, mean = 0.0, sd = sd)
+  
+  # On your marks. Place your bets.
+  weights <- c(1.0)
+  for (t in (w + 1):length(returns)) {
+    cumret <- prod(1 + returns[(t-w):(t-1)]) - 1
+    # Determine if we are risk-on or risk-off:
+    if (cumret < 0) weights <- c(weights, 1.0)
+    else weights <- c(weights, -1.0)
+  }
+  
+  # Turn our hypothetical returns into time series.
+  rets <- returns[w:length(returns)]
+  rets.weighted <- rets * weights
+  dates <- dates <- seq.Date(Sys.Date(), Sys.Date() + 
+                               length(rets) - 1, 1)
+  
+  # Plott he resulting "equity curves".
+  charts.PerformanceSummary(xts(rets, order.by = dates))
+  charts.PerformanceSummary(xts(rets.weighted, order.by = dates))
 }
 
 
-# suppressWarnings(hist(monteCarloSim(sims = 100)))
-# suppressWarnings(compress(marketData()))
+getGlobalStockMarketIndices <- function() {
+  # List of Global Stock Market Indices / ETFs (where index not available)
+  indices <- c("YAHOO/INDEX_DJI", # Dow Jones Industrial (USA)
+               "YAHOO/INDEX_GSPC", # S&P 500 Index (USA)
+               "YAHOO/INDEX_MID", # S&P 400 Mid-cap Index (USA)
+               "YAHOO/INDEX_SML", # S&P 600 Small-cap Index (USA)
+               "YAHOO/NDAQ", # NASDAQ Composite (USA)
+               "YAHOO/INDEX_NYA", # NYSE Composite (USA)
+               "YAHOO/INDEX_RUI", # Russel 1000 Index (USA)
+               "YAHOO/INDEX_RUT", # Russel 2000 Index (USA)
+               "YAHOO/INDEX_RUA", # Russel 3000 Index (USA)
+               "YAHOO/INDEX_XOI", # NYSE AMEX Oil Index (USA)
+               "YAHOO/INDEX_XAX", # NYSE AMEX Composite Index (USA)
+               "YAHOO/INDEX_MERV", # Merval Index (Argentina)
+               "YAHOO/INDEX_BVSP", # Bovespa Index (Brazil)
+               "YAHOO/INDEX_MXX", # Inmex Index (Mexico)
+               "YAHOO/INDEX_MXY", # NYSE ARCA Mexico Index (Mexico)
+               "YAHOO/INDEX_GSPTSE", # S&P TSX Index (Canada)
+               "YAHOO/INDEX_IEQR_IR", # ISEQ General Total Return Index (Ireland)
+               "YAHOO/INDEX_GD_AT", # Athens Composite Index (Greece)
+               "YAHOO/INDEX_FCHI", # CAC40 Index (France)
+               "YAHOO/INDEX_GDAXI", # DAX Index (Germany)
+               "YAHOO/INDEX_OMXC20_CO", # OMX 20 Copenhagen (Denmark)
+               "GOOG/JSE_STX40", # SATRIX Top 40 (South Africa)
+               "GOOG/JSE_STXIND", # SATRIX Industrials (South Africa)
+               "GOOG/JSE_STXFIN", # SATRIX Financials (South Africa)
+               "GOOG/JSE_STXRES", # SATRIX Resources (South Africa)
+               "GOOG/JSE_STXSWX", # SATRIX SWIX (South Africa)
+               "YAHOO/INDEX_CCSI", # EGX 70 Price Index (Egypt) 
+               "YAHOO/INDEX_RTS_RS", # Russia RTS Index (Russia)
+               "YAHOO/INDEX_SSMI", # Swiss Market Index (Switzerland)
+               "YAHOO/INDEX_TA100", # Tel Aviv Top 100 Index (Israel)
+               "YAHOO/INDEX_STOXX50E", # Eurostoxx 50 (Europe)
+               "YAHOO/INDEX_IBEX", # IBEX 35 (Spain)
+               "YAHOO/INDEX_AEX", # AEX Index (Netherlands)
+               "YAHOO/INDEX_BFX", # BEL20 Index (Belgium)
+               "YAHOO/INDEX_ATX", # ATX Index (Austria)
+               "YAHOO/INDEX_OMX", # OMX30 Index (Sweden)
+               "YAHOO/INDEX_N225", # Nikkei 225 (Japan)
+               "YAHOO/INDEX_JPN", # Japan AMEX Index (Japan)
+               "YAHOO/INDEX_SSEC", # Shanghai Composite Index (China)
+               "YAHOO/SS_000300", # CSI300 Index (China)
+               "YAHOO/INDEX_HSI", # Hang Send Index (Hong Kong)
+               "YAHOO/INDEX_KS11", # KOSPI Composite Index (South Korea)
+               "YAHOO/INDEX_BSESN", # BSE 30 Sensitivity Index (SENSEX) (India)
+               "YAHOO/INDEX_NSEI", # Nifty Fifty Index (India)
+               "YAHOO/INDEX_CSE", # Colombo All Share Index (Sri Lanka)
+               "YAHOO/INDEX_NZ50", # New Zealand Stock Exchange (New Zealand)
+               "YAHOO/INDEX_TWII", # Taiwan Weighted Index (Taiwan)
+               "YAHOO/INDEX_AORD", # All Ordinaries Index (Australia)
+               "YAHOO/INDEX_AXJO", # S&P/ASX 200 Index (Australia)
+               "YAHOO/INDEX_JKSE", # Jakarta Composite Index (Indonesia)
+               "YAHOO/INDEX_STI") # Strait Times Index (Singapore)
+  return(indices)
+}
+
+
+getCurrencyPairs <- function() {
+  pairs <- c()
+  for (fx1 in c("USD", "EUR", "GBP")) {
+    for (fx2 in c("ZAR", "BRL", "RUB")) {
+      pair <- paste(fx1, fx2, sep = "")
+      pairs <- c(pairs, paste("CURRFX", pair, sep = "/"))
+    }
+  }
+  pairs <- c(pairs, "CURRFX/USDGBP", "CURRFX/USDEUR", "CURRFX/GBPEUR")
+  return(pairs)
+}
+
