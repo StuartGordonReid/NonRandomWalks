f21f5dabf81f95f3c151fcd06f45fe3d6a89796f
Stuart <stuartgordonreid@gmail.com>
2016-04-29 17:33:48 +0200

    Fixed issue with the monte carlo sim

--- NN Compression based tests - pt2/Py/NeuralNetworks.py
+++ NN Compression based tests - pt2/Py/NeuralNetworks.py
@@ -3,31 +3,45 @@ from keras.layers.core import Dense
 from DataPreprocessor import *
 
 
-def autoencoder_4(real, code, ws, dc, sims, epochs, activation):
+def autoencoder_4(code, ws, dc, sims, epochs, activation, seed):
+    numpy.random.seed(seed=seed)
+
     x = preprocess_data(code, dc, ws)
     l1 = int(ws * 0.9)
     l2 = int(l1 * 0.9)
-    print(str(ws) +
-          ":" + str(l1) +
-          ":" + str(l2))
 
+    train = numpy.asarray(x)
+    model = Sequential()
+    model.add(Dense(l1, input_dim=ws, activation=activation))
+    model.add(Dense(l2, input_dim=l1, activation=activation))
+    model.add(Dense(l1, input_dim=l2, activation=activation))
+    model.add(Dense(ws, input_dim=l1, activation=activation))
+
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    model.fit(train, train, batch_size=32, nb_epoch=epochs, verbose=4)
+    loss, accuracy = model.evaluate(train, train, batch_size=20)
+    mean, stdev = autoencoder_4sig(x, ws, sims, epochs, activation, seed)
+    return accuracy, mean, stdev
+
+
+def autoencoder_4sig(x, ws, sims, epochs, activation, seed):
+    l1 = int(ws * 0.9)
+    l2 = int(l1 * 0.9)
     accuracies = []
     for j in range(0, sims):
-        print("Running simulation " + str(j))
-        if not real:
-            x = benchmark_data(x.size, ws)
+        numpy.random.seed(seed=(seed*(j + 1)))
+        x = benchmark_data(x.size, ws)
         train = numpy.asarray(x)
 
+        numpy.random.seed(seed=seed)
         model = Sequential()
         model.add(Dense(l1, input_dim=ws, activation=activation))
         model.add(Dense(l2, input_dim=l1, activation=activation))
         model.add(Dense(l1, input_dim=l2, activation=activation))
         model.add(Dense(ws, input_dim=l1, activation=activation))
-
         model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
         model.fit(train, train, batch_size=32, nb_epoch=epochs, verbose=4)
-
-        loss, accuracy = model.evaluate(train, train, batch_size=500)
+        loss, accuracy = model.evaluate(train, train, batch_size=20)
         accuracies.append(float(accuracy))
 
     accuracies = numpy.array(accuracies)
@@ -36,13 +50,7 @@ def autoencoder_4(real, code, ws, dc, sims, epochs, activation):
     return mean_accuracy, stdev_accuracy
 
 
-dji_mean, dji_std = autoencoder_4(True, 'YAHOO/INDEX_DJI', 40, 1, 30, 500, 'sigmoid')
-dji_b_mean, dji_b_std = autoencoder_4(False, 'YAHOO/INDEX_DJI', 40, 1, 30, 500, 'sigmoid')
-
-
-print("---------------------------------------")
-print("Results: ")
-print("---------------------------------------")
-print(str(dji_mean), "+-", str(dji_std))
-print(str(dji_b_mean), "+-", str(dji_b_std))
-
+dji_mean, rand_mean, rand_std = autoencoder_4('YAHOO/INDEX_DJI', 32, 2, 55, 1000, 'sigmoid', 1987)
+print("Market score " + str(dji_mean) +
+      "; Random score = " + str(rand_mean) +
+      "; Random stdev = " + str(rand_std))
